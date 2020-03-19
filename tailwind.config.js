582a7960eaf340e861c3f5220fc766c5b2ea9f18
Fadhil Mandaga <fadhil.mandaga@gojek.com>
2020-03-19 16:26:00 +0700

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    fontSize: {
+      '2xs': '.625rem',
+      '3xs': '.5rem',
+      '4xs': '.375rem'
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
